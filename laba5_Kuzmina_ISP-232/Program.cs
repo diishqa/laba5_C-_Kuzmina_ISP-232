8d859fb22b2eb1025b863120aecfeb179d0ef765
diishqa <sfsfgrfhfjhdhdfghdfgh>
2025-09-26 14:58:51 +0300

    5

--- laba5_Kuzmina_ISP-232/Program.cs
+++ laba5_Kuzmina_ISP-232/Program.cs
@@ -47,16 +47,56 @@
             //int y=int.Parse(Console.ReadLine());
             //int x = int.Parse(Console.ReadLine());
             //Sum(y, x);
-            void PrintPerson(string personName, int age)
-            {
-                
-                Console.WriteLine($"Ваше имя: {personName}");
-                Console.WriteLine($"Ваш возраст: {age}");
-            }
-            string name = Console.ReadLine();
-            int aa = int.Parse(Console.ReadLine());
-            PrintPerson(name, aa);
+            //void PrintPerson(string personName, int age)
+            //{
 
+            //    Console.WriteLine($"Ваше имя: {personName}");
+            //    Console.WriteLine($"Ваш возраст: {age}");
+            //}
+            //string name = Console.ReadLine();
+            //int aa = int.Parse(Console.ReadLine());
+            //PrintPerson(name, aa);
+            //string Message()
+            //{
+            //    return "Hello";
+            //}
+            //int Number()
+            //{
+            //    return 10;
+            //}
+            //string message = Message();
+            //Console.WriteLine(message);
+            //Console.WriteLine(Message());
+
+            //string Message() => "hello";
+            //Random random = new Random();
+            //int number = random.Next(1, 100);
+            //System.Console.WriteLine(number);
+
+            //void Sayhello() => Console.WriteLine("Здравствуйте");
+            //void Saygoodbye() => Console.WriteLine("Пока");
+
+            //string GetWeather()
+            //{
+            //    string[] Weather = ["солнечно", "пасмурно", "дождливо"];
+            //    Random random = new Random();
+            //    int index = random.Next(Weather.Length);
+            //    return Weather[index];
+            //}
+            //Console.WriteLine(GetWeather());
+
+            int Factorial(int n)
+            {
+                if (n == 1) return 1;
+                return n * Factorial(n - 1);
+            }
+            int factorial = Factorial(n: 4);
+            int factorial2 = Factorial(n: 5);
+            int factorial3 = Factorial(n: 6);
+            Console.WriteLine(factorial);
+            Console.WriteLine(factorial2);
+            Console.WriteLine(factorial3); 
+            asdasdasd
 
 
 
