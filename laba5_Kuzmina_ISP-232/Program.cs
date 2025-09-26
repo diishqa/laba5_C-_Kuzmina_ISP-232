49b08938914b389a3c8506e082065d88d12eccfe
Student_05 <Student_05@DESKTOP-UNU6F01>
2025-09-26 14:32:01 +0300

    Добавьте файлы проекта.

---
+++ laba5_Kuzmina_ISP-232/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace laba5_Kuzmina_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //void SayHello() =>
+            //    Console.WriteLine("привет");
+
+            //SayHello();
+            //    void Game()
+            //    {
+            //        string myGame = "GTA V";
+            //        Console.WriteLine(myGame);
+            //    }
+            //    Game();
+            //Console.Write("введите свое имя:");
+            //string? name = Console.ReadLine();
+            //Console.Write("введите ваш язык(русский, немецкий, английский)");
+            //string language = Console.ReadLine().ToLower();
+            //void SayHelloRu() => Console.WriteLine($"привет {name}");
+            //void SayHelloEn() => Console.WriteLine($"Hello {name}");
+            //void SayHelloDe() => Console.WriteLine($"Guten Tag {name}");
+
+            //switch ( language )
+            //{
+            //    case "русский": SayHelloRu();
+            //        break;
+            //    case "английский": SayHelloEn();
+            //        break;
+            //    case "немецкий": SayHelloDe();
+            //        break;
+            //    default: 
+            //        Console.WriteLine("не выбрано"); break;
+            //}
+            //void print(string messages)
+            //{
+            //    Console.WriteLine(messages);
+            //}
+            //print("hrloh");
+            //void Sum (int x, int y)
+            //{
+            //    int result = x + y;
+            //    Console.WriteLine ($"результат {result}");
+            //}
+            //Sum(1, 2);
+            //int y=int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //Sum(y, x);
+            void PrintPerson(string personName, int age)
+            {
+                
+                Console.WriteLine($"Ваше имя: {personName}");
+                Console.WriteLine($"Ваш возраст: {age}");
+            }
+            string name = Console.ReadLine();
+            int aa = int.Parse(Console.ReadLine());
+            PrintPerson(name, aa);
+
+
+
+
+
+        }
+
+    }
+}
